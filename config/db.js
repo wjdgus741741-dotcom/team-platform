ff66c045ae0ad62e3425e81f3c0c5738d50159a3
박정현 <wjdgus741741@gmail.com>
2026-06-12 18:51:00 +0900

    connect railway mysql

--- config/db.js
+++ config/db.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'mysql123',
-    database: 'team_platform'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
 });
 
 db.connect((err) => {
